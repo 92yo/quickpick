35a12a99202c6a938660c56a5b154f186f2f57bf
Lina Qaddah <Lina.Qaddah@gmail.com>
2020-02-26 12:03:47 +0200

    setting up gameOver and handled navagations

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import Menu from './screens/Menu/Menu';
 import Game from './screens/Game';
+import GameOver from './screens/GameOver';
 function App() {
   const [userGithub,setUserGithub] = React.useState(null)
+  const [avatarHP,setAvatarHP] = React.useState(1000)
+
   if(userGithub===null){
     return (
       <div>
       <Menu setUserGithub={setUserGithub}/>
       </div>
     );
+  } else if(avatarHP<=0) {
+    return (
+      <div>
+      <GameOver userGithub={userGithub} setUserGithub={setUserGithub} setAvatarHP={setAvatarHP} />
+      </div>
+    );
   }
   return (
     <div>
-    <Game userGithub={userGithub}/>
+    <Game userGithub={userGithub} avatarHP={avatarHP} setAvatarHP={setAvatarHP}/>
     </div>
   );
 }
